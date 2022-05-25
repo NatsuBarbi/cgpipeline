31a0f04a6216d7fb433464d1f47dc0c3be5d36e8
Barbi <barbaros.teker@gmx.net>
2022-05-25 17:00:22 +0200

    refactoring

---
+++ src/main/java/at/fhv/sysarch/lab3/pipeline/Push/PushPipe.java
@@ -0,0 +1,19 @@
+package at.fhv.sysarch.lab3.pipeline.Push;
+
+
+import at.fhv.sysarch.lab3.pipeline.ISink;
+
+// TODO: how can pipes be used for different data types?
+public class PushPipe<T> implements ISink<T> {
+
+    private ISink<T> successor;
+
+    public void setSuccessor(ISink<T> sink) {
+        this.successor = sink;
+    }
+
+    public void write(T face) {
+        this.successor.write(face);
+    }
+
+}
